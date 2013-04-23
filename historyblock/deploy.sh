9ebd8b5f1157a9a2ca35b0799cbf3f11b9800e21
msmith <msmith@techempower.com>
2013-04-23 09:50:14 -0700

    Fixed a line-ending bug.

--- historyblock/deploy.sh
+++ historyblock/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rm -v HistoryBlock.xpi;zip -r HistoryBlock.xpi install.rdf chrome.manifest chrome/ components/ locale/ defaults/ skin/
+rm -v HistoryBlock.xpi
+zip -r HistoryBlock.xpi install.rdf chrome.manifest chrome/ components/ locale/ defaults/ skin/
